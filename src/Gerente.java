8b5201bf8e28238a808b6f3d7d5782e081da0cc9
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-01-09 20:28:33 -0300

    protected, super e sobreescrita de método

---
+++ src/Gerente.java
@@ -0,0 +1,19 @@
+public class Gerente extends Funcionario {
+    private int senha;
+
+    public double getBonificacao () { //reescrita do método getBonificacao da classe superior
+        return super.salario; //super referencia a classe superior
+    }
+
+    public boolean autentica (int senha) {
+        return this.senha == senha;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+}
---
+++ src/TesteGerente.java
@@ -0,0 +1,10 @@
+public class TesteGerente {
+    public static void main(String[] args) {
+        Gerente mateus = new Gerente();
+        mateus.setNome("Mateus");
+        mateus.setCpf("0000000");
+        mateus.setSalario(100);
+        mateus.setSenha(0);
+        System.out.println(mateus.autentica(1));
+    }
+}
