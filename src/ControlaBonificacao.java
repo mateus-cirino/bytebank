780c11064c980f67a67fa27afa824140bfc1d29b
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-01-10 17:27:36 -0300

    polimorfismo

---
+++ src/ControlaBonificacao.java
@@ -0,0 +1,15 @@
+public class ControlaBonificacao {
+    private double soma;
+
+    public void registra (Funcionario funcionario) { //polimorfismo a mesma funcao 'registra' serve tanto para o
+                                                     //funcionario quanto para o gerente.
+                                                     //a funcao recebe a classe mais generica e consegue usar o método
+                                                     //da classe mais especifica (mas tem que ser um método que a generica
+                                                     //tambem possua
+      this.soma += funcionario.getBonificacao();
+    };
+
+    public double getSoma() {
+        return this.soma;
+    }
+}
---
+++ src/TesteControlaBonificacao.java
@@ -0,0 +1,12 @@
+public class TesteControlaBonificacao {
+    public static void main(String[] args) {
+        ControlaBonificacao controle = new ControlaBonificacao();
+        Funcionario funcionario = new Funcionario();
+        funcionario.setSalario(1000.0);
+        Gerente gerente = new Gerente();
+        gerente.setSalario(1000.0);
+        controle.registra(funcionario);
+        controle.registra(gerente);
+        System.out.println(controle.getSoma());
+    }
+}
