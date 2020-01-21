1a48f4c96ab35a9bbd26cda89391a867a092b58d
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-01-21 14:21:47 -0300

    changes

--- src/_funcionario/TesteControlaBonificacao.java
+++ src/_funcionario/TesteControlaBonificacao.java
@@ -3,11 +3,8 @@ package _funcionario;
 public class TesteControlaBonificacao {
     public static void main(String[] args) {
         ControlaBonificacao controle = new ControlaBonificacao();
-        Funcionario funcionario = new Funcionario();
-        funcionario.setSalario(1000.0);
         Gerente gerente = new Gerente();
         gerente.setSalario(1000.0);
-        controle.registra(funcionario);
         controle.registra(gerente);
         System.out.println(controle.getSoma());
     }
