6756d4bef28a268325ef707844502507f1dfaa40
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-01-21 18:17:50 -0300

    composicao

--- src/_funcionario/Gerente.java
+++ src/_funcionario/Gerente.java
@@ -1,7 +1,11 @@
 package _funcionario;
 
 public class Gerente extends Funcionario implements Autenticavel {
-    private int senha;
+    private AutenticacaoUtil util;
+
+    public Gerente () {
+        this.util = new AutenticacaoUtil();
+    }
 
     public double getBonificacao () { //reescrita do método getBonificacao da classe superior
         return super.getSalario(); //super referencia a classe superior
@@ -9,20 +13,16 @@ public class Gerente extends Funcionario implements Autenticavel {
 
     @Override
     public boolean autentica(int senha) {
-        if(this.senha == senha) {
-            return true;
-        } else {
-            return false;
-        }
+        return this.util.autentica(senha);
     }
 
     @Override
     public int getSenha() {
-        return this.senha;
+        return this.util.getSenha();
     }
 
     @Override
     public void setSenha(int senha) {
-        this.senha = senha;
+        this.util.setSenha(senha);
     }
 }
