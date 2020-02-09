0b1ac179b914a1672b36be5bff801c77218ba943
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-02-09 11:33:13 -0300

    utilizando generics na implementacao do Comparator

--- src/br/com/bytebank/banco/modelo/conta/ComparadorContaTitular.java
+++ src/br/com/bytebank/banco/modelo/conta/ComparadorContaTitular.java
@@ -2,12 +2,10 @@ package br.com.bytebank.banco.modelo.conta;
 
 import java.util.Comparator;
 
-public class ComparadorContaTitular implements Comparator{
+public class ComparadorContaTitular implements Comparator<Conta>{
 
 	@Override
-	public int compare(Object o1, Object o2) {
-        Conta conta1 = (Conta) o1;
-        Conta conta2 = (Conta) o2;
+	public int compare(Conta conta1, Conta conta2) {
         return conta1.getTitular().getNome().compareTo(conta2.getTitular().getNome());
 	}
 
