a015a5ea3c3b8cbf9d4050249215231ce72b9f4f
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-02-12 15:30:17 -0300

    aprendendo a lidar com arquivos em java

---
+++ src/br/com/bytebank/banco/teste/TesteLeituraArquivo.java
@@ -0,0 +1,16 @@
+package br.com.bytebank.banco.teste;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class TesteLeituraArquivo {
+    public static void main(String[] args) throws IOException{
+        FileInputStream fis = new FileInputStream("lorem.txt");
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader bis = new BufferedReader(isr);
+
+        System.out.println(bis.readLine());
+    }
+}
