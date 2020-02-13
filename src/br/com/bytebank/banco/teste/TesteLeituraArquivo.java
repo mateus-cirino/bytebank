f5a1bfffa7998ec369c93dcc2198d00ea95e729d
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-02-13 11:14:32 -0300

    leitura e escrita em um arquivo

--- src/br/com/bytebank/banco/teste/TesteLeituraArquivo.java
+++ src/br/com/bytebank/banco/teste/TesteLeituraArquivo.java
@@ -1,21 +1,40 @@
 package br.com.bytebank.banco.teste;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 class TesteLeituraArquivo {
     public static void main(String[] args) throws IOException{
         //inputStream -> leitura de bytes
         //Reader -> leitura de caracteres
 
+        //leitura do arquivo
         FileInputStream fis = new FileInputStream("lorem.txt");
         InputStreamReader isr = new InputStreamReader(fis);
         BufferedReader bis = new BufferedReader(isr);
 
-        System.out.println(bis.readLine());
+        //escrita do arquivo
+        FileOutputStream fos = new FileOutputStream("lorem2.txt");
+        OutputStreamWriter osw = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osw);
 
+        bis.lines().forEach((str) -> {
+            try {
+                bw.write(str);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        //fechando o arquivo de leitura
         bis.close();
+
+        //fechando o arquivo de escrita
+        bw.close();
     }
 }
